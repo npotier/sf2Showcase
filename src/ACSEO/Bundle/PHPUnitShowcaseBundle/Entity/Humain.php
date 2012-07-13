8948a1546482af6a078d125ef557c593c28f1e61
www-data <www-data@nicolas.routerdd0348.com>
2012-07-13 14:37:52 +0200

    creation d'une entite de base : Humain

---
+++ src/ACSEO/Bundle/PHPUnitShowcaseBundle/Entity/Humain.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace ACSEO\Bundle\PHPUnitShowcaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ACSEO\Bundle\PHPUnitShowcaseBundle\Entity\Humain
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="ACSEO\Bundle\PHPUnitShowcaseBundle\Entity\HumainRepository")
+ */
+class Humain
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $prenom
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string $nom
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string $dateNaissance
+     *
+     * @ORM\Column(name="dateNaissance", type="datetime")
+     */
+    private $dateNaissance;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return Humain
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Humain
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set dateNaissance
+     *
+     * @param string $dateNaissance
+     * @return Humain
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->dateNaissance = $dateNaissance;
+        return $this;
+    }
+
+    /**
+     * Get dateNaissance
+     *
+     * @return string 
+     */
+    public function getDateNaissance()
+    {
+        return $this->dateNaissance;
+    }
+
+}
