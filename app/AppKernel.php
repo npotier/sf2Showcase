02b32749e2b9db7d5206d2506d2445eb96913745
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2012-07-16 16:01:06 +0200

    Ajout de FOSUserBundle et de l'entite WebsiteAdmin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,10 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            //
             new ACSEO\Bundle\PHPUnitShowcaseBundle\ACSEOPHPUnitShowcaseBundle(),
+            //
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
